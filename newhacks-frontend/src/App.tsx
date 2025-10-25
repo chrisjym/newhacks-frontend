caff2b674e7b7121b34da5114f1bf8184bb5b3d2
SuperUltraCoolGuy <chris.mong@mail.utoronto.ca>
2025-10-25 13:57:33 -0400

    Finished Map

--- newhacks-frontend/src/App.tsx
+++ newhacks-frontend/src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import Map from './pages/Map';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/map" element={<Map />} />
       </Routes>
     </Router>
   );
--- newhacks-frontend/src/main.tsx
+++ newhacks-frontend/src/main.tsx
@@ -3,9 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import Home from './pages/Home.tsx'
+import Map from './pages/Map.tsx'
+import Planner from './pages/Planner.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Home />
+    <Planner />
   </StrictMode>,
 )
